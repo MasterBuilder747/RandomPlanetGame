1a96f3a4be03925ee619d68b3227fbf13aecb09f
Joseph <jaathunder@yahoo.com>
2019-10-22 15:58:35 -0700

    add planet object, remove test commit, add object declarations for details

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,12 +1,19 @@
 package Main;
 
+import java.util.*;
+
 public class Main {
 
     public static void main(String[] args) {
 
         System.out.println("===Random Planet Game===");
 
-        //testcommit
+        //define objects
+        //this describes the process a bit better
+        Scanner kb = new Scanner(System.in);
+        Random r = new Random();
+        Planet p = new Planet();
+
 
     }
 
---
+++ src/Main/Planet.java
@@ -0,0 +1,21 @@
+package Main;
+
+public class Planet {
+
+    //physical properties
+    int radius;
+    double rotationAngle;
+
+
+    public static double genSeasons(double angle) {
+        double seasonChange = angle * 4;
+
+        //refine formula for calculation; do research
+
+        return seasonChange;
+    }
+
+    double seasons = genSeasons(radius / 2);
+}
+
+
